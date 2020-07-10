7f169b2c485fc93e9a0c1474bcfdb6bcf45b1159
hyeonjoon <hyeonjoon@hyeonjocbookpro>
2020-07-10 17:27:43 +0900

    This is the first commit. A new spring project is created, and a HTTP GET method is tested.

---
+++ timeline/src/main/java/me/timeline/controller/TimelineController.java
@@ -0,0 +1,12 @@
+package me.timeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TimelineController {
+	@GetMapping(path="/login")
+	public String Login() {
+		return "Login";
+	}
+}
