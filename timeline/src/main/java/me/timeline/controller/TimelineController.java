60fabfc5a4885e94df06ec6d801a47c260247c0f
hyeonjoon <hyeonjoon@localhost>
2020-07-13 08:19:32 +0900

    POST method with Json input/ouput works well.

--- timeline/src/main/java/me/timeline/controller/TimelineController.java
+++ timeline/src/main/java/me/timeline/controller/TimelineController.java
@@ -2,12 +2,21 @@ package me.timeline.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TimelineController {
-	@GetMapping(path="/login")
-	public String Login() {
-		return "Login";
+	
+	@PostMapping(path="/signup")
+	public UserInfo SignUp(@RequestBody UserInfo userinfo) {
+		return userinfo;
 	}
+	
+	@PostMapping(path="/signin")
+	public UserInfo SignIn(@RequestBody UserInfo userinfo) {
+		return userinfo;
+	}
+	
 }
+
---
+++ timeline/src/main/java/me/timeline/controller/UserInfo.java
@@ -0,0 +1,14 @@
+package me.timeline.controller;
+
+public class UserInfo {
+	private String email;
+	private String password;
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+}
